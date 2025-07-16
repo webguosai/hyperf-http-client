6a424fc57ae8d95f0e8b1e732d8c029149803913
guosai <501807312@qq.com>
2025-07-16 17:17:46 +0800

    1

--- src/Contract/HttpClientInterface.php
+++ src/Contract/HttpClientInterface.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Webguosai\HyperfHttpClient\Contract;
 
+use Webguosai\HttpClient\Response;
+
 /**
  * Http客户端
- * @method self get(string $url, array $query = [], string|array $headers = [])
- * @method self post(string $url, string|array $data = [], string|array $headers = [])
- * @method self put(string $url, string|array $data = [], string|array $headers = [])
- * @method self patch(string $url, string|array $data = [], string|array $headers = [])
- * @method self delete(string $url, string|array $data = [], string|array $headers = [])
- * @method self head(string $url, string|array $data = [], string|array $headers = [])
- * @method self options(string $url, string|array $data = [], string|array $headers = [])
- * @method self request(string $url, string $method, string|array $data = [], string|array $headers = [])
+ * @method Response get(string $url, array $query = [], string|array $headers = [])
+ * @method Response post(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response put(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response patch(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response delete(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response head(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response options(string $url, string|array $data = [], string|array $headers = [])
+ * @method Response request(string $url, string $method, string|array $data = [], string|array $headers = [])
  */
 interface HttpClientInterface
 {
